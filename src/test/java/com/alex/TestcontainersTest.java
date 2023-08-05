e40bff04c3830cbcc48271cf3e20b9594df50621
Zogymac <sokal.2001@mail.ru>
2023-08-05 10:24:02 +0300

    setup testing

---
+++ src/test/java/com/alex/TestcontainersTest.java
@@ -0,0 +1,17 @@
+package com.alex;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class TestcontainersTest extends AbstractTestContainers {
+
+    @Test
+    void canStartPostgresDB() {
+        assertThat(postgreSQLContainer.isRunning()).isTrue();
+        assertThat(postgreSQLContainer.isCreated()).isTrue();
+    }
+
+
+}
